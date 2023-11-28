d259d9372f14fe1de09db44e2b25f53f0f6cc2f4
Anesu C.J Sibanda <87155971+cjsibanda@users.noreply.github.com>
2023-11-28 04:09:32 -0500

    Update App.tsx
    Added  Utilities expense to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ const App = () => {
     {id:2, description:'bbb', amount:10, category:'Utilities'},
     {id:3, description:'ccc', amount:10, category:'Utilities'},
     {id:4, description:'ddd', amount:10, category:'Utilities'},
+    {id:5, description:'eee', amount:10, category:'Utilities'}
   ]);
 
   return (
