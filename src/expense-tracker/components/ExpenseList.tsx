05a35211a1a1a205db8ad74462f24d6e893e2880
Anesu C.J Sibanda <87155971+cjsibanda@users.noreply.github.com>
2023-12-01 05:13:28 -0500

    Update ExpenseList.tsx

--- src/expense-tracker/components/ExpenseList.tsx
+++ src/expense-tracker/components/ExpenseList.tsx
@@ -20,7 +20,7 @@ const ExpenseList = ({expenses, onDelete}: Props) => {
         <table className="table table-bordered">
           <thead>
             <tr>
-                <th>Description:</th>
+                <th>Description</th>
                 <th>Amount</th>
                 <th>Category</th>
                 <th></th>
