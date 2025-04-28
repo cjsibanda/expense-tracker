42120cba1719b75de3446ccbe1fae266b4d3d836
Anesu C.J Sibanda <87155971+cjsibanda@users.noreply.github.com>
2025-04-28 15:01:44 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,8 @@ const App = () => {
     {id:10, description:'jjj', amount:10, category:'Utilities'},
     {id:11, description:'kkk', amount:10, category:'Utilities'},
     {id:12, description:'lll', amount:10, category:'Utilities'},
-    {id:13, description:'mmm', amount:10, category:'Utilities'}
+    {id:13, description:'mmm', amount:10, category:'Utilities'},
+    {id: 14, description:'mmm', amount:10, category:'Utilities'},
   ]);
 
   return (
