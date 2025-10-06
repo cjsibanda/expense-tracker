33e7e21d99160d61610d6adf397bcb01fc81d6b9
CJ Sibanda <87155971+cjsibanda@users.noreply.github.com>
2025-10-06 05:13:53 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ const App = () => {
     {id:9, description:'iii', amount:10, category:'Utilities'},
     {id:10, description:'jjj', amount:10, category:'Utilities'},
     {id:11, description:'kkk', amount:10, category:'Utilities'},
-    {id:12, description:'lll', amount:10, category:'Utilities'},
   ]);
 
   return (
